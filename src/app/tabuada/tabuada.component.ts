e3bcf83265862b262188931d080a9dd3985d34a4
priscilafraifer <priscilafraifer@gmail.com>
2022-03-04 21:56:26 -0300

    1 file modified, 1 added

--- src/app/tabuada/tabuada.component.ts
+++ src/app/tabuada/tabuada.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabuada',
   templateUrl: './tabuada.component.html',
-  styleUrls: ['./tabuada.component.css']
+  styleUrls: ['./tabuada.component.css'],
 })
 export class TabuadaComponent implements OnInit {
+  x = 10;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
